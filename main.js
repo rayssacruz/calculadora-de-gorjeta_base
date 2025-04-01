67d61c767d492f138ba2e8a7005d86858feb570d
rayssa0433 <rayssa0433@gmail.com>
2025-04-01 01:36:25 +0000

    funçao de calculo da gorjeta

--- main.js
+++ main.js
@@ -40,3 +40,43 @@ let bill = 0
 
     tipPercentage = document.querySelector("#custom-tip").valueAsNumber/100
 }   
+
+function receiveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
+    
+    calculate()
+}
+
+function receiveNumberOfPeopleValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+
+    calculate()
+}
+
+function receiveTipPercentageValue(value){
+buttonSelected = document.querySelector(`#button-${value}`)
+     buttonSelected.classList.add("button-selected")
+     calculate()
+ }
+ 
+ function receiveCustomTipPercentageValue(){
+}
+
+tipPercentage = document.querySelector("#custom-tip").valueAsNumber/100
+calculate()
+
+
+function calculate() {
+if(bill != 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+    let amountStrong = document.querySelector(".amount strong")
+    let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+    amountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+    let totalStrong = document.querySelector(".total strong")
+    let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+    totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+} else {
+    console.log("preencha tudo")
+}
+}
+
