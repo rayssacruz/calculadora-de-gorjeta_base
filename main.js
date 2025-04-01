edbe87387836e62187dbb11247bfe57260fa92c6
rayssa0433 <rayssa0433@gmail.com>
2025-04-01 01:32:40 +0000

    commit da porcentagem

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 let bill = 0
  let tipPercentage = 0
  let numberOfPeople = 0
+ let buttonSelected = null
  
  function receiveBillValue(){
-     bill = document.querySelector("#bill").valueAsNumber
+    bill = document.querySelector("#bill").valueAsNumber
      
  }
  
  function receiveNumberOfPeopleValue(){
-     numberOfPeople = document.querySelector("#people").valueAsNumber
-     console.log(numberOfPeople)
- }
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+    console.log(numberOfPeople)
+    }
+ 
+    function receiveTipPercentageValue(value){
+        if (buttonSelected !== null){
+            buttonSelected.classList.remove("button-selected")
+        }
+    
+        tipPercentage = value / 100
+        console.log(tipPercentage)
+    
+        buttonSelected = document.querySelector(`#button-${value}`)
+        buttonSelected.classList.add("button-selected")
+    }
+    
