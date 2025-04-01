09c12810e2532d739f68f10a1653589aec3125de
rayssa0433 <rayssa0433@gmail.com>
2025-04-01 01:34:41 +0000

    customização

--- main.js
+++ main.js
@@ -18,10 +18,25 @@ let bill = 0
             buttonSelected.classList.remove("button-selected")
         }
     
+        let customTipInput = document.querySelector("#custom-tip")
+    
+        if(customTipInput.value !== ""){
+            customTipInput.value = ""
+        }
+    
+    
         tipPercentage = value / 100
-        console.log(tipPercentage)
-    
-        buttonSelected = document.querySelector(`#button-${value}`)
-        buttonSelected.classList.add("button-selected")
+ 
+     buttonSelected = document.querySelector(`#button-${value}`)
+     buttonSelected.classList.add("button-selected")
+ }
+ 
+ 
+ function receiveCustomTipPercentageValue(){
+    if(buttonSelected != null){
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
     }
-    
+
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber/100
+}   
